4b4a870f9a555810f72c55ce1bc9cfda8f80e47e
Lok Jian Ming <jianminglok@hotmail.com>
2016-01-10 12:12:08 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,11 +1,11 @@
-$(call inherit-product, device/xiaomi/hermes/full_hermes.mk)
+$(call inherit-product, device/xiaomi/hennessy/full_hennessy.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := hermes
-PRODUCT_NAME := cm_hermes
-PRODUCT_BRAND := XiaoMi
-PRODUCT_MODEL := Redmi Note 2
-PRODUCT_MANUFACTURER := XiaoMi
+PRODUCT_DEVICE := hennessy
+PRODUCT_NAME := cm_hennessy
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 3
+PRODUCT_MANUFACTURER := Xiaomi
