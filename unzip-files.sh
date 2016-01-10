8624d927e457aa6cd945ce608effac30a9fd07f2
Lok Jian Ming <jianminglok@hotmail.com>
2016-01-10 12:29:46 +0800

    Update unzip-files.sh

--- unzip-files.sh
+++ unzip-files.sh
@@ -7,7 +7,7 @@ if [ "x$UPDATE_PACKAGE" = "x" ]; then
     exit
 fi
 
-BASE=../../../vendor/xiaomi/hermes/proprietary
+BASE=../../../vendor/xiaomi/hennessy/proprietary
 rm -rf $BASE/*
 
 for FILE in `egrep -v '(^#|^$)' proprietary-blobs.txt`; do
