07ace6c025cc3d02a28e2db09cebb3bce1a5f535
Lok Jian Ming <jianminglok@hotmail.com>
2016-01-10 12:36:15 +0800

    Update Android.mk

--- mtk/hennessy/Android.mk
+++ mtk/hennessy/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE                  := hermes
+LOCAL_MODULE                  := hennessy
 LOCAL_MODULE_TAGS             := optional
 LOCAL_SRC_FILES               := main.c
 LOCAL_SHARED_LIBRARIES        := \
