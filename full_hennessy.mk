1f15d4df116bcb0513756a68d6820336bddff266
Lok Jian Ming <jianminglok@hotmail.com>
2016-01-10 12:27:07 +0800

    Update full_hennessy.mk

--- full_hennessy.mk
+++ full_hennessy.mk
@@ -4,21 +4,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/xiaomi/hermes/device.mk)
+$(call inherit-product, device/xiaomi/hennessy/device.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := xiaomi hermes
+PRODUCT_RELEASE_NAME := xiaomi hennessy
 
 EXTENDED_FONT_FOOTPRINT := true
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.timezone=Europe/Moscow 
+    persist.sys.timezone=Asia/Shanghai 
 
-PRODUCT_DEVICE := hermes
-PRODUCT_NAME := full_hermes
-PRODUCT_BRAND := xiaomi
-PRODUCT_MODEL := Redmi Note 2
-PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_DEVICE := hennessy
+PRODUCT_NAME := full_hennessy
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 3
+PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Boot animation
@@ -26,5 +26,5 @@ TARGET_SCREEN_HEIGHT      := 1920
 TARGET_SCREEN_WIDTH       := 1080
 TARGET_BOOTANIMATION_NAME := 1080
 
-PRODUCT_DEFAULT_LANGUAGE := ru
-PRODUCT_DEFAULT_REGION   := RU
+PRODUCT_DEFAULT_LANGUAGE := en
+PRODUCT_DEFAULT_REGION   := us
